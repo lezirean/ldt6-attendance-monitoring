ae000db55e7fd991fd976fdd804e17d660a2af57
lezirean <73259555+lezirean@users.noreply.github.com>
2023-02-05 13:45:20 +0800

    php file for updating data on teamsrecord.php

---
+++ pages/update_team.php
@@ -0,0 +1,20 @@
+<?php 
+	require 'connection.php';
+
+	if(isset($_POST['editBtn'])) {
+		$team_ID = $_POST['edit-team-id'];
+		$team_name = $_POST['edit-team-name'];
+
+		$query = "UPDATE designation_team SET team_ID='$team_ID', team_name='$team_name' WHERE team_ID='$team_ID'";
+		$result = mysqli_query($connection, $query);
+
+		if($result) {
+	        echo '<script> alert("Data Saved"); </script>';
+	        header('Location: teamsrecord.php');
+	        exit();
+	    } else {
+	        echo '<script> alert("Data Not Updated"); </script>';
+	    }
+
+	}
+?>
